784fc9664709223eac35b0543d5d31599608c764
PhuaLyTanTong-2180609435 <phualytantong@gmail.com>
2025-03-15 09:44:49 +0700

    Phua Ly Tan Tong add dashboard

--- apps/controllers/admin/usercontroller.js
+++ apps/controllers/admin/usercontroller.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res) {
-
-    res.render("./admin/icon-menu");
+    //res.json({"message": "this is admin page"});
+    res.render("./admin/tables/tables");
 });
 
 module.exports = router;
