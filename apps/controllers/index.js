32196e70674f5a839ee2ea520f8ef3687986c041
Nguyen Dai Hiep <97888804+ZNn9@users.noreply.github.com>
2025-03-15 09:03:06 +0700

    Tông

--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 router.use("/home", require(__dirname + "/homecontroller"));
 router.use("/product", require(__dirname + "/productcontroller"));
+router.use("/shop", require(__dirname + "/shopcontroller"));
 router.use("/about", require(__dirname + "/aboutcontroller"));
 router.use("/contact", require(__dirname + "/contactcontroller"));
-router.use("/blog", require(__dirname + "/blogcontroller"));
+router.use("/vegetables", require(__dirname + "/vegetablescontroller"));
 
 router.use("/admin", require(__dirname + "/admin/admincontroller"));
 
