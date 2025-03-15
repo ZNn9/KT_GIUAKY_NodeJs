e4c0c6c062b7738ef65890d02ee1955678b5ceca
Nguyen Dai Hiep <97888804+ZNn9@users.noreply.github.com>
2025-03-15 09:55:22 +0700

    Create productcontroller.js

---
+++ apps/controllers/productcontroller.js
@@ -0,0 +1,31 @@
+var express = require("express");
+const { ObjectId } = require("mongodb");
+var router = express.Router();
+var Product = require("./../entity/product");
+var ProductService = require("./../services/productservice");
+
+router.post("/insert-product", async function(req,res){
+    var productService = new ProductService();
+    var pro = new Product();
+    pro.Name = req.body.Name;
+    pro.Price = req.body.Price;
+    var result =  await productService.insertProduct(pro);
+    res.json({status: true, message:""});
+});
+
+
+router.get("/product-list", async function(req,res){
+    //console.log(req);
+    var pageIndex = Number(req.query.pageIndex) ;
+    var pageSize = Number( req.query.pageSize);
+    console.log("pageIndex:" + pageIndex + " " + "pageSize:" + pageSize);
+    var productService = new ProductService();
+    var product =  await productService.getProductList(pageIndex,pageSize);
+    res.json(product);
+});
+
+
+router.get("/", function(req,res){
+    res.render("shop.ejs");
+});
+module.exports = router;
